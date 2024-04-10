cdeb5ee9081ed8734dd6d29c22e7c8ce16c92dba
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 01:41:12 +0700

    Update path

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -4,7 +4,7 @@ const server = jsonServer.create();
 // allow write operations
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join(process.cwd(), "db.json");
+const filePath = "../db.json";
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
