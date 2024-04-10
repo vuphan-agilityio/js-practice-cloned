8f2631e76ef48f1de61fc892133b33b9964d9807
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 00:56:57 +0700

    Remove logging for path

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -2,13 +2,12 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 
 // allow write operations
-const fs = require("fs")
-const path = require("path")
-console.log("path: ", path);
-const filePath = path.join("db.json")
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db)
+const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
