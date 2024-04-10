004366dc505da4ec84b2e26d6b2020eb3705ea42
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 00:48:51 +0700

    Add logging for path

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 
 // allow write operations
-const fs = require('fs')
-const path = require('path')
-const filePath = path.join('../db.json')
+const fs = require("fs")
+const path = require("path")
+console.log("path: ", path);
+const filePath = path.join("db.json")
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db)
