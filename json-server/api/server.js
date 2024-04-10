daea0da5b021b89cbe16229eb65a40f188d42535
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 01:08:59 +0700

    Add db.json file to api folder

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -4,8 +4,8 @@ const server = jsonServer.create();
 // allow write operations
 const fs = require("fs");
 const path = require("path");
-const filePath = path.join("/db.json");
-const data = fs.readFileSync(process.cwd() + filePath, "utf-8");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
