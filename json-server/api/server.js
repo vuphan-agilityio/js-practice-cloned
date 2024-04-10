c124dcf5924ecb6c013743557c2cf210a689b5e3
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 00:41:33 +0700

    Add server.js to api folder and add vercel.json again

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -1,6 +1,14 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+
+// allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
