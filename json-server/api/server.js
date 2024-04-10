418bcd8570f698412687621df4eb1e20977ec791
Vu Phan <vu.phan@asnet.com.vn>
2024-04-11 01:03:09 +0700

    Add process.cwd() to read path

--- json-server/api/server.js
+++ json-server/api/server.js
@@ -5,7 +5,7 @@ const server = jsonServer.create();
 const fs = require("fs");
 const path = require("path");
 const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
+const data = fs.readFileSync(process.cwd() + filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
 
